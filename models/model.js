587d15d930a23927dc6f33c94321274bc2362eef
Hai Do Minh <minhhai3b@gmail.com>
2016-03-16 22:24:54 +0900

    use pool when start app

--- models/model.js
+++ models/model.js
@@ -1,6 +1,53 @@
 const oracledb = require('oracledb')
 const dbconfig = require('../dbconfig')
 const messages = require('./messages')
+var pool = null
+var openConnections = []
+
+function createPool (cb) {
+  if (pool) return cb()
+  console.log('create a new pool')
+  var authInfo = {
+    user: dbconfig.user,
+    password: dbconfig.password,
+    connectString: dbconfig.connectString
+  }
+  oracledb.createPool(authInfo, function (err, _pool) {
+    if (err) {
+      console.error(messages.ERR_DB_GET_POOL)
+      return cb(err)
+    }
+    console.log('created a new pool successfully')
+    pool = _pool
+    cb()
+  })
+}
+
+function terminatePool (cb) {
+  if (pool) {
+    releaseOpenConnections(function () {
+      pool.terminate(function (err) {
+        console.error(err)
+        pool = null
+        cb()
+      })
+    })
+  } else {
+    cb()
+  }
+}
+
+function releaseOpenConnections (cb) {
+  var counter = openConnections.length
+  if (counter === 0) return cb()
+  openConnections.forEach(function (connection) {
+    connection.release(function (err) {
+      if (err) console.error(err)
+      counter--
+      if (counter === 0) return cb()
+    })
+  })
+}
 
 function execute (query, params, cb, numRows) {
   getConnection(function (err, connection) {
@@ -17,21 +64,14 @@ function execute (query, params, cb, numRows) {
 }
 
 function getConnection (cb) {
-  var authInfo = {
-    user: dbconfig.user,
-    password: dbconfig.password,
-    connectString: dbconfig.connectString
-  }
-  oracledb.createPool(authInfo, function (err, pool) {
-    if (err) {
-      console.error(messages.ERR_DB_GET_POOL)
-      return cb(err)
-    }
+  createPool(function (err) {
+    if (err)  return cb(err)
     pool.getConnection(function (err, connection) {
       if (err) {
         console.error(messages.ERR_DB_GET_CONNECTION)
         return cb(err)
       }
+      openConnections.push(connection)
       cb(null, connection)
     })
   })
@@ -45,6 +85,10 @@ function closeResultSet (connection, resultSet) {
 }
 
 function releaseConnection (connection) {
+  var index = openConnections.indexOf(connection)
+  if (index >= 0) {
+    openConnections.splice(index, 1)
+  }
   connection.release(function (err) {
     if (err) console.error(err)
   })
@@ -66,6 +110,8 @@ function fetchRowsFromRS (connection, resultSet, numRows, cb) {
 }
 
 exports = module.exports = execute
+exports.createPool = createPool
+exports.terminatePool = terminatePool
 exports.getConnection = getConnection
 exports.closeResultSet = closeResultSet
 exports.releaseConnection = releaseConnection
