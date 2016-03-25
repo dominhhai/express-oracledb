67ec5845648d4ce28ac63f5a1232827a62c9c5bc
Hai Do Minh <minhhai3b@gmail.com>
2016-03-26 01:33:04 +0900

    config port

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  port: process.env.PORT || '3000',
   oracledb: {
     user: process.env.NODE_ORACLEDB_USER || 'test',
     password: process.env.NODE_ORACLEDB_PASSWORD || 'test',
@@ -18,15 +19,15 @@ module.exports = {
     ERR_DB_GET_POOL: 'DBに接続できないかPOOLを取得できないため、クエリを実行できません。',
     ERR_DB_GET_CONNECTION: 'DBに接続できないかCONNECTIONを取得できないため、クエリを実行できません。'
   },
+  chart: {
+    slide_time: 10000,
+    refresh_time: 100000,
+    get_data_num_errors: 5
+  },
   shnCds: {
     icc: '2922',
     tsa: '2924',
     lpa: '2926',
     tpi: '2927'
-  },
-  chart: {
-    slide_time: 10000,
-    refresh_time: 100000,
-    get_data_num_errors: 5
   }
 }
