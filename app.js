8d04e70f38b3d1d0fec72459c6084834266bed9d
Hai Do Minh <minhhai3b@gmail.com>
2016-03-25 23:28:20 +0900

    add log4j

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var path = require('path')
-var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var session = require('express-session')
+var log4js = require('log4js')
+var log = log4js.getLogger('app')
 
 var index = require('./routes/index')
 
@@ -13,8 +14,7 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// uncomment after placing your favicon in /public
-app.use(logger('dev'))
+app.use(log4js.connectLogger(log4js.getLogger('http'), { level: 'auto' }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
@@ -40,6 +40,7 @@ app.use(function (req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
+    log.error("Something went wrong:", err)
     res.status(err.status || 500)
     res.render('error', {
       message: err.message,
@@ -51,6 +52,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
+  log.error("Something went wrong:", err)
   res.status(err.status || 500)
   res.render('error', {
     message: err.message,
--- models/db.js
+++ models/db.js
@@ -1,11 +1,12 @@
-const oracledb = require('oracledb')
+const oracledb = {}//require('oracledb')
 const config = require('../config')
+var log = require('log4js').getLogger('db')
 var pool = null
 var openConnections = []
 
 function createPool (cb) {
   if (pool) return cb()
-  console.log('create a new pool')
+  log.info('create a new pool')
   var authInfo = {
     user: config.oracledb.user,
     password: config.oracledb.password,
@@ -13,10 +14,10 @@ function createPool (cb) {
   }
   oracledb.createPool(authInfo, function (err, _pool) {
     if (err) {
-      console.error(config.messages.ERR_DB_GET_POOL)
+      log.error(config.messages.ERR_DB_GET_POOL)
       return cb(err)
     }
-    console.log('created a new pool successfully')
+    log.info('created a new pool successfully')
     pool = _pool
     cb()
   })
@@ -24,12 +25,12 @@ function createPool (cb) {
 
 function terminatePool (cb) {
   if (pool) {
-    console.log('terminate pool')
+    log.info('terminate pool')
     releaseOpenConnections(function () {
       pool.terminate(function (err) {
-        if (err) console.error(err)
+        if (err) log.error(err)
         pool = null
-        console.log('Pool is terminated')
+        log.info('Pool is terminated')
         cb()
       })
     })
@@ -43,7 +44,7 @@ function releaseOpenConnections (cb) {
   if (counter === 0) return cb()
   openConnections.forEach(function (connection) {
     connection.release(function (err) {
-      if (err) console.error(err)
+      if (err) log.error(err)
       counter--
       if (counter === 0) return cb()
     })
@@ -53,7 +54,7 @@ function releaseOpenConnections (cb) {
 function execute (query, params, cb, numRows) {
   getConnection(function (err, connection) {
     if (err) return cb(err)
-    console.log('・query:', query, '\n・params:', params)
+    log.info('・query:', query, '\n・params:', params)
     connection.execute(query, params, { resultSet: true }, function (err, result) {
       if (err) return cb(err.message)
       numRows = numRows || config.oracledb.numRows
@@ -69,7 +70,7 @@ function getConnection (cb) {
     if (err)  return cb(err)
     pool.getConnection(function (err, connection) {
       if (err) {
-        console.error(config.messages.ERR_DB_GET_CONNECTION)
+        log.error(config.messages.ERR_DB_GET_CONNECTION)
         return cb(err)
       }
       openConnections.push(connection)
@@ -80,7 +81,7 @@ function getConnection (cb) {
 
 function closeResultSet (connection, resultSet) {
   resultSet.close(function (err) {
-    if (err) console.error(err.message)
+    if (err) log.error(err.message)
     releaseConnection(connection)
   })
 }
@@ -91,7 +92,7 @@ function releaseConnection (connection) {
     openConnections.splice(index, 1)
   }
   connection.release(function (err) {
-    if (err) console.error(err)
+    if (err) log.error(err)
   })
 }
 
@@ -114,7 +115,7 @@ function simpleExecute (query, params, cb, numRows) {
   var result = []
   execute(query, params, function (err, rows) {
     if (err) {
-      console.error(err)
+      log.error(err)
       return cb([])
     }
     if (rows.length > 0) {
